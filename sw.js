ab0cb73fdbae826ea69a33d25f9c13eb4ab71325
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-23 02:26:28 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-pwa-v271';
+const CACHE='lat-yen-pwa-v272';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
