498e815097fb7279d3c4902bf613a89d56243d6f
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-22 11:49:46 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-pwa-v189';
+const CACHE='lat-yen-pwa-v190';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
