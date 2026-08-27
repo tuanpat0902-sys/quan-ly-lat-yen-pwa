a80fa8b14df388d0eede1d4caab794b7ce89ceea
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-28 01:11:27 +0700

    Bump service worker cache for V3-6 runner delivery

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-fresh-core-v3-authoritative-187';
+const CACHE='lat-yen-fresh-core-v3-authoritative-188';
 const INDEX_KEY='./index.html';
 const PRECACHE_ASSETS=[
   INDEX_KEY,
