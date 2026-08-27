7cc304ef0d1336c3d071dbbf8394a324ee13b1a5
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 15:43:16 +0700

    Bump service worker for Fresh Core V3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-fresh-core-v2-authoritative-174';
+const CACHE='lat-yen-fresh-core-v3-authoritative-175';
 const INDEX_KEY='./index.html';
 const PRECACHE_ASSETS=[
   INDEX_KEY,
