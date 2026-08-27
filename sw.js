f22d456706265443114b10c40a09c7384a975fd2
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-28 01:33:38 +0700

    Bump release cache for V3-6 boot loader

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-fresh-core-v3-authoritative-190';
+const CACHE='lat-yen-fresh-core-v3-authoritative-191';
 const INDEX_KEY='./index.html';
 const PRECACHE_ASSETS=[
   INDEX_KEY,
