b3212b33dbdb30ae9f8c1b8f6732a64054ef9b92
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 17:16:37 +0700

    Start Fresh Core V3-2 Ingredients and Inventory foundation

---
+++ src-v3/domains/ingredients-inventory/index.js
@@ -0,0 +1,4 @@
+export {INGREDIENTS_INVENTORY_CONTRACT} from './ingredients-inventory-contract.js';
+export {createIngredientsInventoryRepository} from './ingredients-inventory-repository.js';
+export {createIngredientsInventoryService} from './ingredients-inventory-service.js';
+export {compareIngredientsInventory} from './parity.js';
