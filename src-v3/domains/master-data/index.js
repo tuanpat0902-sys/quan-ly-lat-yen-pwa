f6cfa918933826cf0e43dde70163fbecabbf5199
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 14:25:36 +0700

    Pass organization context into V3 Master Data parity

--- src-v3/domains/master-data/index.js
+++ src-v3/domains/master-data/index.js
@@ -2,9 +2,9 @@ import {createMasterDataRepository} from './master-data-repository.js';
 import {createMasterDataService} from './master-data-service.js';
 import {MASTER_DATA_CONTRACT} from './master-data-contract.js';
 
-export function createMasterDataDomain({gateway,cache,events,v2Adapter}){
+export function createMasterDataDomain({gateway,cache,events,v2Adapter,getOrgId}){
   const repository=createMasterDataRepository({gateway});
-  const service=createMasterDataService({repository,cache,events,v2Adapter});
+  const service=createMasterDataService({repository,cache,events,v2Adapter,getOrgId});
   return Object.freeze({id:MASTER_DATA_CONTRACT.id,contract:MASTER_DATA_CONTRACT,repository,service});
 }
 
