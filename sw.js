d52fe0651dcf2876ddd61616d2e544398296af19
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 18:24:08 +0700

    Release Ver 3.0.11 with bounded V3-2 technical validation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-fresh-core-v3-authoritative-185';
+const CACHE='lat-yen-fresh-core-v3-authoritative-186';
 const INDEX_KEY='./index.html';
 const PRECACHE_ASSETS=[
   INDEX_KEY,
