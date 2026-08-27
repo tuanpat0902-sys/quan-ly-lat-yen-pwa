c311b380d60ffab04bdc62e32552891d59d31bc4
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 23:19:21 +0700

    Keep V3-6 domain transport-agnostic

--- src-v3/domains/employees/index.js
+++ src-v3/domains/employees/index.js
@@ -2,13 +2,13 @@ import {createEmployeesRepository} from './employees-repository.js';
 import {createEmployeesService} from './employees-service.js';
 import {EMPLOYEES_CONTRACT} from './employees-contract.js';
 
-export function createEmployeesDomain({gateway,v2Adapter,events}={}){
-  const repository=createEmployeesRepository({gateway});
+export function createEmployeesDomain({source,v2Adapter,events}={}){
+  const repository=createEmployeesRepository({source});
   const service=createEmployeesService({repository,v2Adapter,events});
   return Object.freeze({id:EMPLOYEES_CONTRACT.domain,contract:EMPLOYEES_CONTRACT,repository,service});
 }
 
 export {EMPLOYEES_CONTRACT,EMPLOYEES_MIGRATION_GUARD} from './employees-contract.js';
-export {createEmployeesRepository,normalizeEmployeeDirectoryRow,EMPLOYEES_DIRECTORY_RPC,EMPLOYEES_DIRECTORY_FIELDS} from './employees-repository.js';
+export {createEmployeesRepository,normalizeEmployeeDirectoryRow,EMPLOYEES_DIRECTORY_FIELDS} from './employees-repository.js';
 export {createEmployeesService} from './employees-service.js';
 export {compareEmployeeDirectory,EMPLOYEES_PARITY_FIELDS} from './parity.js';
