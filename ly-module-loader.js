93deba4f07594969efdc86ea8bb8d8e479feb85f
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 18:18:54 +0700

    Load V3-2 technical validation Settings UI

--- ly-module-loader.js
+++ ly-module-loader.js
@@ -53,7 +53,7 @@
     cloudRealtime:{src:'./ly-cloud-realtime.js?v=20260824.5',test:()=>window.__lyUnifiedCloudRealtime?.version==='2026.08.24.5'},
     warehouseDeleteUX:{src:'./ly-warehouse-delete-ux.js?v=20260824.3',test:()=>window.__lyWarehouseDeleteUX?.version==='2026.08.24.3'},
     settingsUIBridge:{src:'./ly-settings-ui-bridge.js?v=20260827.2',test:()=>window.__lySettingsUIBridge?.version==='2026.08.27.2'},
-    settings:{src:'./ly-settings-enhancements.js?v=20260827.5',test:()=>window.__lyNotificationMaster?.version==='2026.08.27.5'},
+    settings:{src:'./ly-settings-enhancements.js?v=20260827.6',test:()=>window.__lyNotificationMaster?.version==='2026.08.27.6'},
     settingsUI:{src:'./ly-settings-ui.js?v=20260823.1',test:()=>!!window.__lySettingsUIModule},
     branding:{src:'./ly-branding-sync.js?v=20260823.2',test:()=>!!window.__lyBrandingSync},
     heavyPanels:{src:'./ly-heavy-panels.js?v=20260823.1',test:()=>!!window.__lyHeavyPanels},
