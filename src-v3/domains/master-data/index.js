5f45744ad3002421ec24fc17e01029ee5b1a3655
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 14:21:31 +0700

    Add V3 Master Data domain entrypoint

---
+++ src-v3/domains/master-data/index.js
@@ -0,0 +1,18 @@
+import {createMasterDataRepository} from './master-data-repository.js';
+import {createMasterDataService} from './master-data-service.js';
+import {MASTER_DATA_CONTRACT} from './master-data-contract.js';
+
+export function createMasterDataDomain({gateway,cache,events,v2Adapter}){
+  const repository=createMasterDataRepository({gateway});
+  const service=createMasterDataService({repository,cache,events,v2Adapter});
+  return Object.freeze({id:MASTER_DATA_CONTRACT.id,contract:MASTER_DATA_CONTRACT,repository,service});
+}
+
+export async function activate(context){
+  const domain=createMasterDataDomain(context);
+  return Object.freeze({
+    domain,
+    refreshShadow:()=>domain.service.refreshShadow(),
+    deactivate:async()=>{}
+  });
+}
