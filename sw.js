ce22f0719708a69ac9768425611234e99b980db9
tuanpat0902-sys <tuanpat0902@gmail.com>
2026-08-27 17:07:39 +0700

    Bump service worker cache to 183

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lat-yen-fresh-core-v3-authoritative-182';
+const CACHE='lat-yen-fresh-core-v3-authoritative-183';
 const INDEX_KEY='./index.html';
 const PRECACHE_ASSETS=[
   INDEX_KEY,
